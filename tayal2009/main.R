d5ab8a34aec6b4e8eb0486b09d3d0c39fbeff21e
Luis Damiano <damiano.luis@gmail.com>
2017-08-17 00:44:40 -0300

    tayal2009 top node analysis

--- tayal2009/main.R
+++ tayal2009/main.R
@@ -12,7 +12,7 @@ files <- c('../data/2007.05.01.G.TO.RData',
            '../data/2007.05.04.G.TO.RData',
            '../data/2007.05.07.G.TO.RData')
 
-files <- c('../data/2007.05.01.G.TO.RData',
+# files <- c('../data/2007.05.01.G.TO.RData',
            '../data/2007.05.02.G.TO.RData')
 
 series <- do.call(rbind, lapply(files, function(f) {
@@ -50,21 +50,20 @@ n.seed = 9000
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-stan.model = 'tayal2009/stan/hhmm-tayal2009.stan'
+stan.model = 'tayal2009/stan/hhmm-tayal2009-lite.stan'
 stan.data = list(
   T = T.length,
   K = K,
   L = L,
   sign = ifelse(x < L + 1, 1, 2),
-  x = ifelse(x < L + 1, x, x - L)
-)
+  x = ifelse(x < L + 1, x, x - L))
 
 stan.fit <- stan(file = stan.model,
                  model_name = stan.model,
                  data = stan.data, verbose = T,
                  iter = n.iter, warmup = n.warmup,
                  thin = n.thin, chains = n.chains,
-                 cores = n.cores, seed = n.seed)#w, init = init_fun)
+                 cores = n.cores, seed = n.seed)
 
 n.samples = (n.iter - n.warmup) * n.chains
 
@@ -78,12 +77,12 @@ launch_shinystan(stan.fit)
 
 # Extraction
 alpha_tk <- extract(stan.fit, pars = 'alpha_tk')[[1]]
-gamma_tk <- extract(stan.fit, pars = 'gamma_tk')[[1]]
-zstar_t  <- extract(stan.fit, pars = 'zstar_t')[[1]]
+# gamma_tk <- extract(stan.fit, pars = 'gamma_tk')[[1]]
+# zstar_t  <- extract(stan.fit, pars = 'zstar_t')[[1]]
 
 class.fil <- apply(apply(alpha_tk, c(2, 3), median), 1, which.max)
-class.smo <- apply(apply(gamma_tk, c(2, 3), median), 1, which.max)
-class.vit <- apply(zstar_t, 2, function(z) { which.max(tabulate(z)) })
+# class.smo <- apply(apply(gamma_tk, c(2, 3), median), 1, which.max)
+# class.vit <- apply(zstar_t, 2, function(z) { which.max(tabulate(z)) })
 
 # Summary -----------------------------------------------------------------
 options(digits = 2)
@@ -112,6 +111,62 @@ matrix(summary(stan.fit,
                pars = c('phi_k'),
                probs = c(0.10, 0.50, 0.90))$summary[, c(1, 3, 4, 5, 6)][, 4],
        K, L, TRUE)
+
+# In-sample analysis ------------------------------------------------------
+plot_stateprobability(alpha_tk, alpha_tk, 0.8)
+
+zig$topstate <- ifelse(class.fil == 1 | class.fil == 4, 1, -1)
+
+topstate.chg <- zig$topstate != lag(zig$topstate)
+topstate.chg[1] <- TRUE
+
+top <- zig[topstate.chg == TRUE, ]
+top$end <- c(as.numeric(tail(top$start, -1)) - 1, 1)
+top$ret <- (as.numeric(tdata[top$end, 1]) - as.numeric(tdata[top$start, 1])) / as.numeric(tdata[top$start, 1])
+
+head(top)
+
+zigzag$start  <- c(1, head((1:nrow(price))[topstate.chg], -1))
+
+zigzag$end <- lag(zigzag$start, -1) - 1
+zigzag$end[nrow(zigzag)] <- nrow(price)
+
+top <-
+
+# in-sample expected trade return of runs and reversals
+# Average percentage return per run/reversal switch
+# Assign meaning bull R1 > R2
+
+# hist(features) per top node
+
+
+# Out-of-sample analysis --------------------------------------------------
+# Viterbi most likely path
+# 1. Average percentage return per run/reversal switch after zigzag is complete (uses initial price of the zigzag)
+# 2. Chisq to compare if distributions of two states are similar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Top Node  I:  Run/Bull        States 1 (negative zigzag) and 2 (positive zigzag)
 # Top Node II:  Reversal/Bear   States 3 (positive zigzag) and 4 (negative zigzag)
